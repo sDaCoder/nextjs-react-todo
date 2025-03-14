020584df70cd58c493494eff94da79f97f2819e3
sDaCoder <supravadutta_sDa@outlook.com>
2025-03-14 10:14:29 +0530

    Modified the default / route

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import { startOfDay } from "date-fns"
 import { redirect } from "next/navigation"
 
 const page = () => {
-  redirect(`/${startOfDay(new Date()).getTime()}`)
+  redirect(`/${new Date().getTime()}`)
 }
 
 export default page
