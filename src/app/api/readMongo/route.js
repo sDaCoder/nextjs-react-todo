5a3d6275404d78f13c700f2a4a8d41aa420332ea
sDaCoder <supravadutta_sDa@outlook.com>
2025-03-23 01:44:51 +0530

    Added the functionality of deleting the items from the database using post request

--- src/app/api/readMongo/route.js
+++ src/app/api/readMongo/route.js
@@ -1,18 +1,18 @@
 import { NextResponse } from "next/server";
 import { configDotenv } from "dotenv";
 import { MongoClient } from "mongodb";
+import { ObjectId } from "mongodb";
 
-
+const URI = process.env.MONGO_URI
+const client = new MongoClient(URI)
 export async function GET() {
-    const URI = process.env.MONGO_URI
-    const client = new MongoClient(URI)
     try {
         await client.connect()        
         const database = client.db("todos")
         const collection = database.collection("todo-01");
         const data = await collection.find({}).toArray();
         const todos = data.map(doc => ({
-            id: doc.$id,
+            id: doc._id.toString(),
             todo: doc.todo,
             desc: doc.desc,
             isDone: doc.isDone,
@@ -31,6 +31,35 @@ export async function GET() {
         return NextResponse.json({
             status: 'failure',
             message: 'Could not read the data from MongoDB',
+            error
+        })
+    } finally {
+        await client.close()
+    }
+}
+
+export async function POST(request) 
+{
+    try {
+        const body = await request.json()
+        const {id} = body
+        console.log(body, id);
+
+        await client.connect()
+        const database = client.db("todos")
+        const collection = database.collection("todo-01");
+        const result = await collection.deleteOne({ _id: new ObjectId(id) });
+
+        return NextResponse.json({
+            status: 'success',
+            message: 'Reading the data from MongoDB',
+            result
+        })
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({
+            status: 'failure',
+            message: 'Could not delete the data',
             error
         })
     }
--- src/components/TodoCard/TodoCard.js
+++ src/components/TodoCard/TodoCard.js
@@ -53,6 +53,22 @@ const TodoCard = ({ item }) => {
 
   const handleDeleteTodo = () => {
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== item.id));
+    (async () => {
+      try {
+        const res = await fetch(`/api/readMongo`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(item),
+        })
+        const data = await res.json()
+        console.log(data)
+      } catch(err)
+      {
+        console.log(err)
+      }
+    })()
 
     showNormalToast(
       `Deleting Your Task`,
